1182d7b605d301631dd948fc478b7b0664508502
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2020-02-28 14:59:50 +0530

    Add files via upload

---
+++ LinearRegression.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+x=10*np.random.rand(100)
+y=3*x+np.random.rand(100)
+print(x,y)
+plt.scatter(x,y)
+plt.show()
