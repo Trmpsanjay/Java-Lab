23730e515e4f1d51449006b9eeeccf7cdd2482d3
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2020-03-03 00:39:59 +0530

    CodingNinja

---
+++ NumberPattern1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class NumberPattern1 {
+	
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i=0,j=0;
+        for(i=0;i<=n;i++){
+            for(j=0;j<i;j++){
+                System.out.print("1");
+            }
+        System.out.println();
+        }
+		// Write your code here
+
+	}
+}
