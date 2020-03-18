a26df2fc3ee878111b88a95ce3c107e0ca8cb4e7
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2020-03-18 15:29:26 +0530

    need correction for <= than 3 element

---
+++ BinarySearch.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class function{
+	public void sorting(int arr[]){
+		for(int i=0;i<arr.length;i++){
+			for(int j=0;j<arr.length-1;j++){
+				if(arr[j]>=arr[j+1]){
+					int temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+				}
+			}
+		}
+	}
+	public void binarySearch(int arr[],int key){
+		int first=0,last=arr.length-1;
+		while(first<last){
+			int mid=(first+last)/2;
+			if(arr[mid]==key){
+				int m=mid+1;
+				System.out.println("\n "+key+"found at position"+m);
+				break;
+			}
+			else if(arr[mid]>key){
+				last=mid-1;
+			}
+			else
+				first=mid+1;
+		}
+	}
+}
+
+class BinarySearch{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		function f=new function();
+		System.out.println("Enter the size of the array");
+		int size=sc.nextInt();
+		int arr1[]=new int[size];
+		for(int i=0;i<size;i++){
+			arr1[i]=sc.nextInt();
+		}
+		System.out.print("enter the number to be searched");
+		int serach=sc.nextInt();
+		f.sorting(arr1);
+		System.out.println("sorted arrays are");
+		for(int i=0;i<size;i++)
+			System.out.print(arr1[i]+" ");
+		f.binarySearch(arr1,serach);
+	}
+}
